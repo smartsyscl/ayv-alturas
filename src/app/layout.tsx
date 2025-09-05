3ea56c5aebc407119ca24a4e0aedeb2769ce687d
jean.andresst <jean.andresst@gmail.com>
2025-09-05 20:20:37 +0000

    Fix: utils.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils"; // Import the cn utility
+import { cn } from "src/app/lib/utils"; // 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
